a14624a1c2e58290f583e0485be2c9f898daceb8
Marisame <rulivas26@gmail.com>
2022-05-20 17:50:50 -0500

    first commit

---
+++ createTablesPkm.sql
@@ -0,0 +1,31 @@
+-- Drop the tables in case it exists
+
+DROP TABLE IF EXISTS types CASCADE;
+DROP TABLE IF EXISTS classfications CASCADE;
+DROP TABLE IF EXISTS pokemons CASCADE;
+
+-- Create the tables
+
+CREATE TABLE types (
+  typeID INTEGER PRIMARY KEY,
+  type VARCHAR(15) NOT NULL
+);
+
+CREATE TABLE classfications (
+  classficationID INTEGER PRIMARY KEY,
+  classfication VARCHAR(55) NOT NULL
+);
+
+CREATE TABLE pokemons(
+  pokemonID INTEGER PRIMARY KEY,
+  name VARCHAR(15) NOT NULL,
+  hp INTEGER NOT NULL,
+  attack INTEGER NOT NULL,
+  defense INTEGER NOT NULL,
+  sp_attack INTEGER NOT NULL,
+  sp_defense INTEGER NOT NULL,
+  speed INTEGER NOT NULL,
+  type INTEGER REFERENCES types(typeID),
+  classfication INTEGER REFERENCES classfications(classficationID)
+);
+
