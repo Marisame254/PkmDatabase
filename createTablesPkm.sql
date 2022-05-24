cd969b9d425587ecbce2e5dd2c0394ce622dc7bc
Marisame <rulivas26@gmail.com>
2022-05-24 15:52:58 -0500

    create the againsts table

--- createTablesPkm.sql
+++ createTablesPkm.sql
@@ -17,10 +17,32 @@ CREATE TABLE classfications (
   classfication VARCHAR(55) NOT NULL
 );
 
-create TABLE abilities (
+CREATE TABLE abilities (
   abilityID INTEGER PRIMARY KEY,
   ability VARCHAR(30) NOT NULL
 );
+
+CREATE TABLE againsts (
+  againstID INTEGER PRIMARY KEY,
+  against_bug REAL NOT NULL,
+  against_dark REAL NOT NULL,
+  against_dragon REAL NOT NULL,
+  against_electric REAL NOT NULL,
+  against_fairy REAL NOT NULL,
+  against_figth REAL NOT NULL,
+  against_fire REAL NOT NULL,
+  against_flying REAL NOT NULL,
+  against_ghost REAL NOT NULL,
+  against_grass REAL NOT NULL,
+  against_ground REAL NOT NULL,
+  against_ice REAL NOT NULL,
+  against_normal REAL NOT NULL,
+  against_poison REAL NOT NULL,
+  against_psychic REAL NOT NULL,
+  against_rock REAL NOT NULL,
+  against_steel REAL NOT NULL,
+  against_water REAL NOT NULL,
+) 
 
 CREATE TABLE pokemons(
   generation INTEGER NOT NULL,
