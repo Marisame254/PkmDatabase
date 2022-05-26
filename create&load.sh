663f22a9d56c20269adc9617a924525ef3a56da2
Marisame <rulivas26@gmail.com>
2022-05-26 14:00:26 -0500

    Create pokemons.csv with abilities, and correct the script

--- create&load.sh
+++ create&load.sh
@@ -14,11 +14,6 @@ echo "\COPY classfications FROM 'data/classfications.csv' DELIMITERS ',' \
             CSV HEADER;" | psql -U nue -d pokemondb
 
 
-echo "Populating abilities table"
-echo "\COPY abilities FROM 'data/abilities.csv' DELIMITERS ',' \
-            CSV HEADER;" | psql -U nue -d pokemondb
-
-
 echo "Populating againsts table"
 echo "\COPY againsts FROM 'data/againsts.csv' DELIMITERS ',' \
             CSV HEADER;" | psql -U nue -d pokemondb 
